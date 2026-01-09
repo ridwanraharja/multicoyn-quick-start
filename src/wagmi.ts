81f1bdc5bb6c1a26626de0b36df1fe4e94198891
ridwanraharja <9612621ridwan@gmail.com>
2026-01-09 21:03:06 +0700

    feat: default lisk testnet chain

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,9 +1,9 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, polygon, optimism, arbitrum, sepolia, liskSepolia } from 'wagmi/chains';
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { liskSepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: 'Multicoyn NFT Marketplace',
-  projectId: 'YOUR_PROJECT_ID', // Get from WalletConnect Cloud (https://cloud.walletconnect.com)
-  chains: [mainnet, polygon, optimism, arbitrum, sepolia, liskSepolia],
+  appName: "Multicoyn NFT Marketplace",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [liskSepolia],
   ssr: false,
 });
